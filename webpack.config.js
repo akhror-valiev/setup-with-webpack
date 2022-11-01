36159c5ba7b6e024d2ebb9102d8c7de06ce8ba47
akhror-valiev <ahror.valiev@gmail.com>
2022-11-01 11:30:45 +0500

    Create webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+    mode: 'development',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        publicPath: '/',
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+
+};
